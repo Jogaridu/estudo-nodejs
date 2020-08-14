3c0e57ecd0dc3a0eae02932c755122ea8489b653
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-08-14 15:07:06 -0300

    Desafio com Programação Orientada ao Objeto

---
+++ DESAFIO#POO/Dado.js
@@ -0,0 +1,18 @@
+class Dado {
+    constructor(numFace) {
+        this.numeroFace = numFace;
+    }
+
+    rolar() {
+
+        const faceEscolhida = Math.floor(Math.random() * this.numeroFace);
+
+        console.log(`Dentre um dado de ${this.numeroFace} faces, caiu a face de lado ${faceEscolhida}`);
+    }
+}
+
+const faceSeis = new Dado(6);
+const faceDez = new Dado(10);
+
+faceDez.rolar();
+faceSeis.rolar();
